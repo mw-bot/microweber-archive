a2470abe5d754427a8b51bcbaab3a2b5e94dec00
Peter Ivanov <peter@ooyes.net>
2012-06-05 19:14:06 +0300

    update

--- userfiles/modules/forms/mail_form/templates/colorful.php
+++ userfiles/modules/forms/mail_form/templates/colorful.php
@@ -8,7 +8,10 @@ name: Colorful
 
 description: Colorful
 
+custom_markup: false
+
 */
 
 ?>
-test Colorful
+<? $skip_view = true; ?>
+test Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorfultest Colorful
