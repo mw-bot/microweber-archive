40a4c2ec6457e4f4845cf07c65fe89202e81693a
Peter Ivanov <peter@ooyes.net>
2012-07-22 10:06:39 +0300

    update

---
+++ application/includes/toolbar/toolbar_socket.php
@@ -0,0 +1,33 @@
+<script type="text/javascript">
+ var serverURL =  'ws://192.168.0.3:9300';
+ 
+ 
+ window.mw = window.mw ? window.mw : {};
+ 
+ 
+ mw.multi_edit = {
+	 user_id : '<? print user_id() ?>',
+	 session_id : '<? print sid() ?>',
+	 user_name : '<? print user_name() ?>',
+	data_element_id: '',
+	mutation_summaries: '',
+	utc_date: '',
+	data_element_id_content: '',
+	  mouse_pos_x : '',
+	   mouse_pos_y : ''
+ }
+ 
+ 
+ 
+ </script>
+<div class="mw-multi-user-sidebar">
+
+<div class="mw-multi-user-sidebar-userlist">
+    
+      
+
+</div>
+    
+     <textarea id="mw-socket-log"></textarea> 
+
+</div><!-- /end .mw -->
