3e97a55e184f5b4d1589cd2f63bd589a27d35c18
Peter Ivanov <info@ooyes.net>
2012-08-02 08:51:11 +0300

    update

---
+++ userfiles/modules/highlight_code_config.php
@@ -0,0 +1,10 @@
+<?
+
+$config = array();
+$config['name'] = "Highlight Code";
+$config['author'] = "Microweber";
+$config['no_cache'] = true;
+$config['ui'] = true;
+$config['categories'] = "other"; 
+
+
